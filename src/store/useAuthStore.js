ec0202524f0cc9861c7ffd3c52229d8b9039961b
jacobgoodboy <jacobgoodboy9@gmail.com>
2025-02-27 04:50:37 -0600

    signup useauthstore finished

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -2,27 +2,38 @@ import { create } from "zustand";
 import { axsiosInstance } from "../lib/axios";
 import { data } from "react-router-dom";
 
-export const useAuthStore = create((set)=>({
-    authUser:null,
-    isSginingUp:false,
-    isLoggingIn:false,
-    isUpdatingProfile:false,
-    isCheckingAuth:true,
+export const useAuthStore = create((set) => ({
+    authUser: null,
+    isSginingUp: false,
+    isLoggingIn: false,
+    isUpdatingProfile: false,
+    isCheckingAuth: true,
 
-    checkAuth:async ()=>{
-        try{
+    checkAuth: async () => {
+        try {
             const res = await axsiosInstance.get('/get/check')
-            set({authUser:res.data})
-        }catch(err){
-            set({authUser:null})
-            console.log('error in check auth fr',err)
-        }finally{
-            set({isCheckingAuth:false})
+            set({ authUser: res.data })
+        } catch (err) {
+            set({ authUser: null })
+            console.log('error in check auth fr', err)
+        } finally {
+            set({ isCheckingAuth: false })
         }
     },
 
-    signup:async(data) =>{
-        
+    signup: async (data) => {
+        set({ isSigningUp: true });
+        try {
+            const res = await axiosInstance.post("/auth/signup", data);
+            set({ authUser: res.data });
+            toast.success("Account created successfully");
+            get().connectSocket();
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isSigningUp: false });
+        }
+
     }
 }))
 
