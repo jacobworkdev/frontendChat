06af0cb4768280751424be33af03d58449990f54
jacobgoodboy <jacobgoodboy9@gmail.com>
2025-02-27 03:06:44 -0600

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
-const App = () =>{
-  return(
-    <div className="text-red-500">Hello</div>
+import Navbar from "./components/Navbar"
+import { Routes, Route } from "react-router-dom"
+const App = () => {
+  return (
+
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/settings' element={<SettingsPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+
+      </Routes>
+    </div>
+
   )
+
+
 }
 export default App
